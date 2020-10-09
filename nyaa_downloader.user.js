240c96a947c79ba8ef92272fc2685d36ba9b6553
Johnsen92 <Johnsen92@bitbucket.org>
2020-10-09 14:50:19 +0200

    added README.md description and metadata

--- nyaa_downloader.user.js
+++ nyaa_downloader.user.js
@@ -1,21 +1,26 @@
 // ==UserScript==
-// @name     nyaa.si
-// @version  1
-// @grant    none
-// @require  https://code.jquery.com/jquery-3.5.1.min.js
-// @include	 *nyaa.si*
+// @name        nyaa.si download all button
+// @description Adds a "Download all" button next to the top right nav bar
+// @version     1
+// @grant       none
+// @license			MIT
+// @require     https://code.jquery.com/jquery-3.5.1.min.js
+// @include     *nyaa.si/*
+// @exclude			*nyaa.si/rules
+// @exclude			*nyaa.si/help
+// @exclude			*nyaa.si/upload
 // ==/UserScript==
 
 function magnetAll(){
-  $("a").each(function() {
-    var match = this.href.match("magnet:")
-    if(match != null){
-      console.log(this.href);
+  $(".torrent-list a").each(function() {
+    if(this.href.match("magnet:") != null){
     	this.click();
     }
 	})
 }
 
-$(".nav.navbar-nav.navbar-right").append("<li><button style=\"margin-top: 9px\" class=\"btn btn-primary\" id=\"magnetall\">Download all</button></li>");
-document.getElementById("magnetall").addEventListener('click', magnetAll, false);
+if(document.getElementById("magnet-all") == null){
+	$(".nav.navbar-nav.navbar-right").append("<li><button style=\"margin-top: 9px\" class=\"btn btn-primary\" id=\"magnet-all\">Download all</button></li>");
+	document.getElementById("magnet-all").addEventListener('click', magnetAll, false);
+}
 
